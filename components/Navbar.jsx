f21a9f2214287e6b778dbf8fb001be4799e32c49
Shumaila Kayani <shumailakayani059@gmail.com>
2022-06-12 12:39:31 +0200

    added logo to public folde

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
     <div className='fixed w-full h-20 shadow-xl z-[100]'>
       <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
         <Image  
-        src="/../public/assets/logo.png"
+        src="/../public/logo.png"
          alt='/'
          width="140"
          height="85"/>
