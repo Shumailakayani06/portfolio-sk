103c46b2e61a95736c6519887ed97dba404cc944
Shumailakayani06 <shumailakayani059@gmail.com>
2023-06-05 10:43:07 +0200

    css

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -13,7 +13,7 @@ import ProjectItem from './ProjectItem'
 const Projects = () => {
   return (
     <div id='projects' className='w-full mt-20'>
-        <div className='max-w-[1240px] mx-auto px-2 py-16'>
+        <div className='max-w-[1240px] mx-auto px-2 pt-16 py-16'>
             <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects</p>
             <h2>What I Have Built</h2>
             <div className='grid md:grid-cols-2 gap-8'>
