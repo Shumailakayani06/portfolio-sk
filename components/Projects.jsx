ecfebdf339623fa316767af40a8e15b840fda521
Shumailakayani06 <shumailakayani059@gmail.com>
2023-06-07 09:31:06 +0200

    reinstalled package.json

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -8,7 +8,7 @@ import jsCa from '../public/assets/jscaImg.png'
 import pokedex from '../public/assets/pokedex.png'
 import Link from 'next/link'
 import ProjectItem from './ProjectItem'
-import memorycard from '../public/assets/memory'
+import memorycard from '../public/assets/memory.png'
 
 const Projects = () => {
   return (
